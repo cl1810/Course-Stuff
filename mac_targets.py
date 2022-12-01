2c4fdb90a29d6eb3f847082c9bcb26b807c50243
cl1810 <115414575+cl1810@users.noreply.github.com>
2022-12-01 15:17:49 -0500

    Add files via upload

---
+++ mac_targets.py
@@ -0,0 +1,23 @@
+inputUser = input('Type a list of target MACs separated by spaces ')
+print("\n")
+
+targetListFound = inputUser.split()
+targetListLost = inputUser.split()
+
+#manipulate the list to add the required prefixes
+
+foundString = "devicefound=" 
+lostString = "devicelost="
+
+print("Here are your .conf inputs douche. \n")
+
+for n in targetListFound:
+	newFoundString = foundString + n
+	print(newFoundString)
+
+print("\n")
+
+for n in targetListLost:
+	newLostString = lostString + n
+	print(newLostString)
+
